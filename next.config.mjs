361586d766340104b9c579d4e747d09c6ec8c3a0
Joel <joelbearn@gmail.com>
2024-04-24 10:19:08 +0100

    fix: changed next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-	reactStrictMode: true,
-	experimental: {
-		appDir: true,
-		serverComponentsExternalPackages: ['mongoose']
-	},
-};
+
+const nextConfig = {
+    images: {
+        domains: ['utfs.io'],
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'utfs.io',
+            port: ''
+        }]
+    },
+    experimental: {
+        appDir: true,
+        esmExternals: "loose", // <-- add this
+        serverComponentsExternalPackages: ["mongoose"] // <-- and this
+    },
+    // and the following to enable top-level await support for Webpack
+    webpack: (config) => {
+      config.experiments = {
+        topLevelAwait: true
+      };
+      return config;
+    },
+  }
+
+
